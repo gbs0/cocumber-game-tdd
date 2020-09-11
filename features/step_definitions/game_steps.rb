00069e6d120487e6ecaced1e702add2034527da4
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2020-09-11 10:29:11 -0300

    tdd now its working

--- features/step_definitions/game_steps.rb
+++ features/step_definitions/game_steps.rb
@@ -1,8 +1,10 @@
+require_relative '../../game.rb'
+
 When("Start new game") do
   @game = Game.new
   @game.start
 end
 
-Then("I should be told {string}") do |expected_answer|
-  expect(@actual_answer).to eq(expected_answer)
+Then("I should be seeing {string}") do |text|
+  expect(@game.output).to include(text)
 end
